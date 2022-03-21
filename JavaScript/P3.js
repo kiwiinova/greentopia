5a7ab5de687178cf93e263bba08a45bd1f2e079a
jesseythach <94651491+jesseythach@users.noreply.github.com>
2022-03-20 21:15:45 -0400

    Add files via upload

--- JavaScript/P3.js
+++ JavaScript/P3.js
@@ -1,20 +1,20 @@
-// JavaScript Document// fetching details from dom
+// JavaScript Document
+
 const price = document.getElementById('price');
 const product = price.dataset.product;
 const quantity = document.getElementById('quantity');
 const orignalPrice = price.innerText;
 localStorage.setItem(`oriPrice-${product}`,orignalPrice);
 
-// fetchin details form local storage
+// fetching details from local storage
 if(localStorage.getItem(`quantity-${product}`) && localStorage.getItem(`newPrice-${product}`)){
     price.innerText = localStorage.getItem(`newPrice-${product}`);
     quantity.value = localStorage.getItem(`quantity-${product}`);
 }
 
-// adding event listner
 quantity.addEventListener('input',updatePrice);
 
-// update price funtion
+//update price funtion
 function updatePrice(){
     let value = quantity.value;
     let newPrice = Number(localStorage.getItem(`oriPrice-${product}`) * value).toFixed(2);

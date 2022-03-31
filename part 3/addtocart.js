0c0f251a921d479dff75bb60de286820be58e6e9
Shamma Markis <95432232+kiwiinova@users.noreply.github.com>
2022-03-31 02:00:52 -0400

    Update addtocart.js

--- part 3/addtocart.js
+++ part 3/addtocart.js
@@ -1,5 +1,4 @@
 var cart = [];
-//  var sum= 0.0;
         $(function () {
             if (localStorage.cart)
             {
@@ -9,8 +8,6 @@ var cart = [];
         });
         
             function addToCart(index)    {
-//                 var sum= 0.0;
-//              $('#cart-content > tbody > tr').each(function() {
             // var $img1 = $('<img width="30px" height="30px" src="' + addTocart.data("image") + '"/>').css({"position":"fixed","z-index":"999"});
             var img1 = $('#item-list').find('.item-image').attr('src');
             // var img1 = $(this).data('image');
@@ -19,14 +16,7 @@ var cart = [];
             var name = $('h4').text();
             var qty = $('.count').val();
             var amount = (qty*price);
-//             var total = $('#totalPrice').text();
-//             var itemTot = parseFloat(qty*price)
 
-//             sum+=amount;
-//             $(this).find('.amount').text(''+amount);
-            
-//              });
-//              $('#totalPrice').text('$'+ sum.toFixed(2));
 
             // update qty if product is already present
             for (var i in cart) {
@@ -77,29 +67,15 @@ var cart = [];
                              item.Quantity + "</td><td>" + (item.Quantity*item.Price).toFixed(2) + " $" + "</td><td>"
                              + "<button onclick='deleteItem(" + i + ")' class='delete'>X</button></td></tr>";
                 $("#cart-content tbody").append(row);
-                sum+=parseFloat((item.Quantity*item.Price));
-//                 var new = localStorage.getItem(sum);
-//                     $('#totalPrice').val('$'+ sum);
-                buttonActivator();
+                sum+=parseFloat((item.Quantity*item.Price)).toFixed(2);
             }
             $('#totalPrice').text('$'+ sum);
             $(".delete").css({"border": "1px solid red", "border-radius": "50%", "padding": "5px 10px", "text-decoration": "none", "color": "red", "width": "30px", "height": "30px", "flex-direction" : "column", "justify-content" : "center" , "align-item" : "center", "cursor": "pointer"});
            
     }  
-    let buttonActivator = () => {
-     let removeButtons =  document.querySelectorAll(".remove")
+    
+$('#clear-cart').click(function(){
+$('#cart-content tbody').remove();
+});
 
-removeButtons.forEach(el => {
-     el.addEventListener("click", (e)=>{
-          let row = e.target.parentNode.parentNode;
-          document.querySelectorAll("table#cart-content tbody")[0].removeChild(row)
-     })
-})
 
-}
-/// For clearing the entire cart
-document.querySelector("a#clear-cart").addEventListener("click", ()=>{
-     let rows = document.querySelectorAll("table#cart-content tbody")[0].childNodes
-     while(rows)
-          document.querySelectorAll("table#cart-content tbody")[0].removeChild(rows[0])  
-})

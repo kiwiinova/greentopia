7a6743a0b636ba7818b68cabad4a68e6112cb547
Tanya-STY <95064474+Tanya-STY@users.noreply.github.com>
2022-04-29 14:24:51 -0400

    Add files via upload

---
+++ src1/admin.js
@@ -0,0 +1,40 @@
+// function setFormMessage(formElement, type, message) {
+//     const messageElement = formElement.querySelector(".form__message");
+
+//     messageElement.textContent = message;
+//     messageElement.classList.remove("form__message--success", "form__message--error");
+//     messageElement.classList.add(`form__message--${type}`);
+// }
+
+// function setInputError(inputElement, message) {
+//     inputElement.classList.add("form__input--error");
+//     inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+// }
+
+// function clearInputError(inputElement) {
+//     inputElement.classList.remove("form__input--error");
+//     inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+// }
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Admin" && password == "Admin"){
+alert ("Login successfully");
+window.location.href = "../SOEN287/index1.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
